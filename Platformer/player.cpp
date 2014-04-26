1607b975978100bceabde9483bc49b9708557154
adrian17 <adrian.wielgosik@gmail.com>
2014-04-26 16:57:34 +0200

    Introduced player SpriteState

--- Platformer/player.cpp
+++ Platformer/player.cpp
@@ -11,17 +11,25 @@ namespace {
 	const double maxVelX = 0.325;
 }
 
+bool operator<(const Player::SpriteState &a, const Player::SpriteState &b){
+	if (a.motionType != b.motionType)
+		return a.motionType < b.motionType;
+	if (a.horizontalFacing != b.horizontalFacing)
+		return a.horizontalFacing < b.horizontalFacing;
+	return false;
+}
+
 Player::Player(Graphics &graphics, int x, int y):
 	x(x), y(y)
 {
-	sprite.reset(new AnimatedSprite(graphics, "content/myChar.bmp", 0, 0, tileSize, tileSize, 15, 3));
+	initSprites(graphics);
 }
 
 Player::~Player(){
 }
 
 void Player::update(int dt){
-	sprite->update(dt);
+	sprites[getSpriteState()]->update(dt);
 
 	x += velX * dt;
 	velX += accX * dt;
@@ -32,17 +40,41 @@ void Player::update(int dt){
 }
 
 void Player::draw(Graphics &graphics){
-	sprite->draw(graphics, (int)round(x), (int)round(y));
+	sprites[getSpriteState()]->draw(graphics, (int)round(x), (int)round(y));
 }
 
 void Player::startMovingLeft(){
 	accX = -walkingAcceleration;
+	horizontalFacing = LEFT;
 }
 
 void Player::startMovingRight(){
 	accX = walkingAcceleration;
+	horizontalFacing = RIGHT;
 }
 
 void Player::stopMoving(){
 	accX = 0.0;
+}
+
+Player::SpriteState Player::getSpriteState(){
+	return SpriteState(
+		accX == 0.0 ? STANDING : WALKING,
+		horizontalFacing);
+}
+
+void Player::initSprites(Graphics &graphics){
+	sprites[SpriteState(STANDING, LEFT)] = std::unique_ptr<Sprite>(new Sprite(graphics, "content/myChar.bmp",
+		0, 0,
+		tileSize, tileSize));
+	sprites[SpriteState(WALKING, LEFT)] = std::unique_ptr<Sprite>(new AnimatedSprite(graphics, "content/myChar.bmp",
+		0, 0,
+		tileSize, tileSize, 15, 3));
+	sprites[SpriteState(STANDING, RIGHT)] = std::unique_ptr<Sprite>(new Sprite(graphics, "content/myChar.bmp",
+		0, tileSize,
+		tileSize, tileSize));
+	sprites[SpriteState(WALKING, RIGHT)] = std::unique_ptr<Sprite>(new AnimatedSprite(graphics, "content/myChar.bmp",
+		0, tileSize,
+		tileSize, tileSize, 15, 3));
+
 }
--- Platformer/player.h
+++ Platformer/player.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <map>
 #include <memory>
 
 class Graphics;
@@ -18,9 +19,24 @@ public:
 	void startMovingRight();
 	void stopMoving();
 private:
+	enum MotionType { STANDING, WALKING };
+	enum HorizontalFacing { LEFT, RIGHT };
+
+	struct SpriteState{
+		SpriteState(MotionType motionType = STANDING, HorizontalFacing horizontalFacing = LEFT) :
+			motionType(motionType), horizontalFacing(horizontalFacing) {};
+		MotionType motionType;
+		HorizontalFacing horizontalFacing;
+	};
+	friend bool operator<(const SpriteState &a, const SpriteState &b);
+	SpriteState getSpriteState();
+	void initSprites(Graphics &graphics);
+
 	double x, y;
 	double velX=0.0, velY=0.0;
 	double accX=0.0, accY=0.0;
-	std::unique_ptr<Sprite> sprite;
+	HorizontalFacing horizontalFacing = LEFT;
+
+	std::map<SpriteState, std::unique_ptr<Sprite>> sprites;
 };
 
