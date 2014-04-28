e2c66e2b00554cc19e2eef554c7200a6d4cdf333
adrian17 <adrian.wielgosik@gmail.com>
2014-04-28 02:03:19 +0200

    Finish Polar Star offsets

--- Platformer/weapons/polarStar.cpp
+++ Platformer/weapons/polarStar.cpp
@@ -5,9 +5,16 @@
 
 namespace {
 	const std::string spritePath = "content/Arms.bmp";
-	const int polarStarIndex = 3;
+	const int polarStarIndex = 2;
 	const Units::Pixel gunWidth = Units::gameToPixel(3 * Units::halfTile);
 	const Units::Pixel gunHeight = Units::gameToPixel(2 * Units::halfTile);
+
+	const Units::Pixel horizontalOffset = 0;
+	const Units::Pixel upOffset = Units::tileToPixel(2);
+	const Units::Pixel downOffset = Units::tileToPixel(4);
+
+	const Units::Pixel leftOffset = 0;
+	const Units::Pixel rightOffset = Units::tileToPixel(1);
 }
 
 PolarStar::PolarStar(Graphics &graphics)
@@ -20,6 +27,12 @@ PolarStar::~PolarStar()
 }
 
 void PolarStar::draw(Graphics &graphics, HorizontalFacing horizontalFacing, VerticalFacing verticalFacing, Units::Game x, Units::Game y){
+	if (horizontalFacing == LEFT)
+		x -= Units::halfTile;
+	if (verticalFacing == UP)
+		y -= Units::halfTile / 2;
+	if (verticalFacing == DOWN)
+		y += Units::halfTile / 2;
 	spriteMap[SpriteState(horizontalFacing, verticalFacing)]->draw(graphics, x, y);
 }
 
@@ -32,7 +45,12 @@ void PolarStar::initSprites(Graphics &graphics){
 }
 
 void PolarStar::initSprite(Graphics &graphics, const SpriteState spriteState){
+	Units::Pixel srcY = (spriteState.horizontalFacing == LEFT) ? leftOffset : rightOffset;
+	if (spriteState.verticalFacing == HORIZONTAL) srcY += horizontalOffset;
+	else if (spriteState.verticalFacing == UP) srcY += upOffset;
+	else if (spriteState.verticalFacing == DOWN) srcY += downOffset;
+
 	spriteMap[spriteState] = std::shared_ptr<Sprite>(new Sprite(graphics, spritePath,
-		polarStarIndex*gunWidth, gunHeight,
+		polarStarIndex*gunWidth, srcY,
 		gunWidth, gunHeight));
 }
